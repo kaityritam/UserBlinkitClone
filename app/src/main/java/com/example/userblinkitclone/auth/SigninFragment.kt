ad659dca41ab900f18dda8a9a6e9198e3016bb88
ritamkaity <ritam.kaity@gmail.com>
2025-04-26 14:09:18 +0530

    Sign In Fragment is Created for Number Input

--- app/src/main/java/com/example/userblinkitclone/auth/SigninFragment.kt
+++ app/src/main/java/com/example/userblinkitclone/auth/SigninFragment.kt
@@ -1,19 +1,97 @@
 package com.example.userblinkitclone.auth
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.userblinkitclone.R
+import com.example.userblinkitclone.databinding.FragmentSigninBinding
 
 class SigninFragment : Fragment() {
+    private lateinit var binding: FragmentSigninBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_signin, container, false)
+        binding = FragmentSigninBinding.inflate(layoutInflater)
+
+
+        val view = binding.root
+        getUserNumber()
+
+        view.post {
+            // Set the system UI visibility for full screen and immersive mode
+            view.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
+            // Make the status bar and navigation bar transparent
+            requireActivity().window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
+            // Set transparent status and navigation bars
+            val window = requireActivity().window
+            window.statusBarColor = android.graphics.Color.TRANSPARENT
+            window.navigationBarColor = android.graphics.Color.TRANSPARENT
+        }
+        return view
+
+    }
+
+    private fun getUserNumber() {
+        binding.editTextPhone.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+
+            }
+
+            override fun onTextChanged(
+                number: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                val length = number?.length
+
+                if (length == 10) {
+                    binding.continueButton.setBackgroundColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.green
+                        )
+                    )
+                } else {
+                    binding.continueButton.setBackgroundColor(
+                        ContextCompat.getColor(
+                            requireContext(),
+                            R.color.grey
+                        )
+                    )
+                }
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
     }
 
 }
